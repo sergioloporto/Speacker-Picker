8e0ae24d5c88520195b8852964743cb424818a5f
Sergio Lo Porto <sergio.loporto@limango.com>
2023-02-26 22:36:55 +0100

    fixd typo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Speacker picker</h1>
+        <h1>Speaker picker</h1>
         <InsertNames />
       </header>
     </div>
